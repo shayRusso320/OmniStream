4026d84e1d8fd8cb2b69645b48a95f4acf51c07a
shayRusso320 <shayrusso111@gmail.com>
2026-03-16 18:40:57 +0200

    move to remote state file

--- backend.tf
+++ backend.tf
@@ -1,5 +1,9 @@
 terraform {
-  backend "local" {
-    path = "terraform.tfstate"
+  backend "s3" {
+    bucket         = "omnistream-tf-state"
+    key            = "global/terraform.tfstate"
+    region         = "eu-north-1"
+    dynamodb_table = "dynamo-tf-locks"
+    encrypt        = true
   }
 }
